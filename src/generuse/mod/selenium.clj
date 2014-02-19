55b2694f2bf90d3e9caeeab0048676ab640a6150
Jayaraj Poroor <jayaraj.poroor@gmail.com>
2014-02-19 12:02:31 +0530

    fixed licenses to LGPL

--- src/generuse/mod/selenium.clj
+++ src/generuse/mod/selenium.clj
@@ -1,3 +1,12 @@
+; Copyright (c) Jayaraj Poroor. All rights reserved.
+; The use and distribution terms for this software are covered by the
+; GNU Lesser General Public License 3.0 
+; (http://www.gnu.org/copyleft/lesser.html)
+; which can be found in the file lgpl-3.0.html at the root of this distribution.
+; By using this software in any fashion, you are agreeing to be bound by
+; the terms of this license.
+; You must not remove this notice, or any other, from this software.
+
 (ns generuse.mod.selenium
 	(:gen-class)
    	(:use [generuse.lib.exec :only (deref-eval)])	
