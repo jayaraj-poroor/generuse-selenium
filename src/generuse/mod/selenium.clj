a35bbf1c0dae30e964fd570a8a97ef8836aa7305
Jayaraj Poroor <jayaraj.poroor@gmail.com>
2014-02-27 01:32:03 +0530

    fixed bugs. read-address-bar implemented

--- src/generuse/mod/selenium.clj
+++ src/generuse/mod/selenium.clj
@@ -20,21 +20,21 @@
 											ctx globals & more]
 	(let [driver (FirefoxDriver.)]
 		(dosync
-		    (alter (:value target-eval) assoc :value driver) 
+		    (alter (:value target-eval) assoc :value {:driver driver}) 
 			(alter globals assoc "address-bar"
-				(ref {:type :web_address-bar :value driver})
+				(ref {:type :web_address-bar :value {:driver driver}})
 			)
 		)
 	)
 )
 
-(def shows-browser_ {:names ["shows"] :target-type :web_browser})
-(defn ^{:axon shows-browser_} shows-browser[target-eval param-evals 
+(def show-browser_ {:names ["show", "is-shown?"] :target-type :web_browser})
+(defn ^{:axon show-browser_} show-browser[target-eval param-evals 
 											ctx globals & more]
-	(let [driver (:value (deref-eval target-eval))]
-		(if (.contains (.toString driver) "null")
-			{:value false :type :boolean :pass false}
-			{:value true :type :boolean :pass true}			
+	(let [driver (get-in (deref-eval target-eval) [:value :driver] )]
+		(if (and driver (not (.contains (.toString driver) "null")))
+			{:value true :type :boolean :pass true}
+			{:value false :type :boolean :pass false}			
 		)
 	)
 )
@@ -44,9 +44,42 @@
 (defn ^{:axon input-address-bar_} input-address-bar[target-eval param-evals 
 														ctx globals & more]
 	(let [target (deref-eval target-eval)
-		  driver (:value target)
+		  driver (:driver (:value target))
 		  url    (:value (deref-eval (:with param-evals)))
 		 ]
-		(.get driver url)
+		 (if driver
+			(.get driver url)
+		 )
+	)
+)
+
+
+(def input_ {:names ["input"] :target-type :web_html})
+(defn ^{:axon input-address-bar_} input-address-bar[target-eval param-evals 
+														ctx globals & more]
+	(let [target (deref-eval target-eval)
+		  driver (:driver (:value target))
+		  url    (:value (deref-eval (:with param-evals)))
+		 ]
+		 (if driver
+			(.get driver url)
+            (throw (ex-info "Browser not open."))			
+		 )
+	)
+)
+
+(def read-address-bar_ {:names ["read"] :target-type :web_address-bar})
+(defn ^{:axon read-address-bar_} read-address-bar[target-eval param-evals 
+														ctx globals & more]
+	(let [target (deref-eval target-eval)
+		  driver (:driver (:value target))
+		 ]
+		 (if driver
+		 	{:value
+				(.executeScript driver "return location.href;" (into-array []))
+			 :type :string 
+			}
+            (throw (ex-info "Browser not open."))			
+		 )
 	)
 )
